0d48f81e34080da6092dd1301ee66cc4e67bff34
EvgeniiyaR <tulpansik@mail.ru>
2023-08-03 02:32:51 +0300

    feat: add header layout for main page

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,32 @@
+import { useState } from 'react';
+import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import './Header.css';
+import logo from '../../images/logo.svg';
+
 const Header = () => {
+  const [isLogin, setLogin] = useState(false);
+  const navigate = useNavigate();
+
   return (
-    <>
-    </>
+  <header className={`header ${!isLogin && "header_type_main"}`}>
+    <Link className="header__link header__link_type_logo" to="/"><img className="header__logo" src={logo} alt="logo"></img></Link>
+    {isLogin ?
+      <nav>
+        <ul className="header__list">
+          <li><Link className="header__link" to="/movies">Фильмы</Link></li>
+          <li><Link className="header__link" to="/saved-movies">Сохраненные фильмы</Link></li>
+          <li><Link className="header__link" to="/profile">Аккаунт</Link></li>
+        </ul>
+      </nav>
+      :
+      <nav>
+      <ul className="header__list">
+          <li><Link className="header__link" to="/signup">Регистрация</Link></li>
+          <li><Link className="header__link header__link_type_login" to="/signin">Войти</Link></li>
+        </ul>
+    </nav>
+    }
+  </header>
   )
 }
 
