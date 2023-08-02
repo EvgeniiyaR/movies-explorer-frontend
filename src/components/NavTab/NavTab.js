706e06462b2d43bc38379e497255ff989ccbc0d6
EvgeniiyaR <tulpansik@mail.ru>
2023-08-03 02:31:16 +0300

    feat: add layout in navtab component

--- src/components/NavTab/NavTab.js
+++ src/components/NavTab/NavTab.js
@@ -1,7 +1,18 @@
+//компонент с навигацией по странице «О проекте»
+import { Link } from 'react-router-dom';
+import './NavTab.css';
+
 const NavTab = () => {
   return (
-    <>
-    </>
+    <section className="nav-page" aria-label="navigation inter page">
+      <nav>
+        <ul className="nav-page__list">
+          <li><Link className="nav-page__link" to="#">О проекте</Link></li>
+          <li><Link className="nav-page__link" to="#">Технологии</Link></li>
+          <li><Link className="nav-page__link" to="#">Студент</Link></li>
+        </ul>
+      </nav>
+    </section>
   )
 }
 
