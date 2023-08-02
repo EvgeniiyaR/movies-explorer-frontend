612b22196fd5cab61e84d6a1c58f6ebdac97d2cd
EvgeniiyaR <tulpansik@mail.ru>
2023-08-03 02:29:43 +0300

    feat: add routes in app component and layout

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,12 +1,32 @@
+import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import Main from '../Main/Main';
+import Header from '../Header/Header';
+import Register from '../Register/Register';
+import Login from '../Login/Login';
+import NotFound from '../NotFound/NotFound';
+import Footer from '../Footer/Footer';
+import Movies from '../Movies/Movies';
+import SavedMovies from '../SavedMovies/SavedMovies';
+import Profile from '../Profile/Profile';
 
 function App() {
   return (
-    <>
-      <h1>Hello, world</h1>
-      <Main />
-    </>
+    <div className="body">
+      <div className="page">
+        <Header />
+        <Routes>
+          <Route path="/" element={<Main />} />
+          <Route path="/movies" element={<Movies />} />
+          <Route path="/saved-movies" element={<SavedMovies />} />
+          <Route path="/profile" element={<Profile />} />
+          <Route path="/signup" element={<Register />} />
+          <Route path="/signin" element={<Login />} />
+          <Route path="*" element={<NotFound />} />
+        </Routes>
+        <Footer />
+      </div>
+    </div>
   );
 }
 
