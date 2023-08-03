ff57aff854c5f4560f40d4cd0746b2afe0df4385
EvgeniiyaR <tulpansik@mail.ru>
2023-08-04 02:17:45 +0300

    feat: add layout in aboutme component

--- src/components/AboutMe/AboutMe.js
+++ src/components/AboutMe/AboutMe.js
@@ -1,9 +1,21 @@
-//компонент с информацией о студенте
+import { Link } from 'react-router-dom';
+import './AboutMe.css';
+import photo from '../../images/photo.png';
 
 const AboutMe = () => {
   return (
-    <>
-    </>
+    <section className="about-me">
+      <h2 className="about-me__title">Студент</h2>
+      <div className="about-me__info-container">
+        <div className="about-me__info">
+          <p className="about-me__name">Евгения</p>
+          <p className="about-me__about">Фронтенд-разработчик</p>
+          <p className="about-me__description">Мир веб-разработки привлекает меня своей творческой составляющей и возможностью создавать удивительные пользовательские интерфейсы. Я хочу применить свой опыт и умения, чтобы создавать динамичные и интуитивно понятные веб-приложения.</p>
+        <Link className="about-me__link" to="https://github.com/EvgeniiyaR" target="_blank" rel="noreferrer">Github</Link>
+      </div>
+      <img className="about-me__photo" src={photo} alt="student"></img>
+      </div>
+    </section>
   )
 }
 
