96ddcffcd005617583d692438c558e6ad601f3ff
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 21:01:59 +0300

    feat: add layout in profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,20 +1,45 @@
+import { useNavigate } from 'react-router-dom';
+import './Profile.css';
 import Form from '../Form/Form';
 import Input from '../Input/Input';
 import Button from '../Button/Button';
 
-const Profile = () => {
-  const handleSubmitEdit = () => {}
+const Profile = ({ setIsLoggedIn }) => {
+  const navigate = useNavigate();
+  const handleSubmitEdit = () => {};
+  const handleSubmitLogout = () => {
+    setIsLoggedIn(false);
+    navigate('/', {replace: true});
+  };
 
   return (
-    <>
-      <h1>Привет, Евгения</h1>
+    <section className="profile">
+      <h1 className="profile__welcome">Привет, Евгения!</h1>
       <Form className="profile__form" name="profile" onSubmit={handleSubmitEdit}>
-        <Input classNameInput="profile__input" type="text" />
-        <Input classNameInput="profile__input" type="email" />
-        <Button className="profile__button-edit" type="submit" />
-        <Button className="profile__button-logout" type="button" />
+        <div className="profile__container-inputs">
+          <Input
+            classNameInput="profile__input"
+            classNameLabel="profile__label profile__label_type_name"
+            type="text"
+            name="profile-name"
+            placeholder="Имя"
+            label="Имя"
+            value="Евгения" />
+          <Input
+            classNameInput="profile__input"
+            classNameLabel="profile__label profile__label_type_email"
+            type="email"
+            name="profile-email"
+            placeholder="E-mail"
+            label="E-mail"
+            value="pochta@yandex.ru" />
+        </div>
+        <div className="profile__container-btns">
+          <Button className="profile__button profile__button_type_edit" type="submit" text="Редактировать" />
+          <Button className="profile__button profile__button_type_logout" type="button" text="Выйти из аккаунта" onClick={handleSubmitLogout}/>
+        </div>
       </Form>
-    </>
+    </section>
   )
 }
 
