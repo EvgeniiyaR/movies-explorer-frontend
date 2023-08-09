a4727b7b3668e7f76fd61a0670b63629de19cea9
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:19:36 +0300

    fix: move nav from header to navigation component

--- src/components/Navigation/Navigation.js
+++ src/components/Navigation/Navigation.js
@@ -1,8 +1,49 @@
-//компонент, который отвечает за меню навигации на сайте
+import { useState } from 'react';
+import { Link, Route, Routes, useNavigate, NavLink } from 'react-router-dom';
+import Button from '../Button/Button';
 
 const Navigation = () => {
+  const [isOpenBurger, setIsOpenBurger] = useState(false);
+
+  const setActive = ({ isActive }) => isActive ? "header__link header__link_type_auth header__link_active" : "header__link header__link_type_auth";
+  const setProfile = ({ isActive }) => isActive ? "header__link header__link_type_auth header__link_type_profile header__link_active" : "header__link header__link_type_auth header__link_type_profile";
+  const navigate = useNavigate();
+
+  const handleOpenBurger = () => {
+    setIsOpenBurger(true);
+  }
+
+  const handleCloseBurger = () => {
+    setIsOpenBurger(false);
+  }
+
   return (
     <>
+      <Button className="header__burger" type="button" onClick={handleOpenBurger} />
+      <nav className={`header__nav-mobile ${isOpenBurger && "header__nav-mobile_active"}`}>
+        <div className="header__container-mobile">
+          <Button className="header__burger-exit" type="button" onClick={handleCloseBurger} />
+          <ul className="header__list-mobile">
+            <li><NavLink className={setActive} to="/">Главная</NavLink></li>
+            <li><NavLink className={setActive} to="/movies">Фильмы</NavLink></li>
+            <li><NavLink className={setActive} to="/saved-movies">Сохраненные фильмы</NavLink></li>
+          </ul>
+          <div className="header__container-link header__container-link_type_mobile">
+            <NavLink className={setProfile} to="/profile">Аккаунт</NavLink>
+            <div className="header__icon-profile"></div>
+          </div>
+        </div>
+      </nav>
+      <nav className="header__nav-desktop">
+        <ul className="header__list header__list_type_auth">
+          <li><NavLink className={setActive} to="/movies">Фильмы</NavLink></li>
+          <li><NavLink className={setActive} to="/saved-movies">Сохраненные фильмы</NavLink></li>
+          <li className="header__container-link">
+            <NavLink className={setActive} to="/profile">Аккаунт</NavLink>
+            <div className="header__icon-profile"></div>
+          </li>
+        </ul>
+      </nav>
     </>
   )
 }
