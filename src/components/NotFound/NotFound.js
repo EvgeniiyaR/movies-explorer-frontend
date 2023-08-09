dcfa62fc65a75bb13be835a625580f620cc47fb8
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 20:58:03 +0300

    feat: add layout in notfound component

--- src/components/NotFound/NotFound.js
+++ src/components/NotFound/NotFound.js
@@ -1,8 +1,19 @@
+import { useNavigate } from 'react-router-dom';
+import Button from '../Button/Button';
+import './NotFound.css';
+
 const NotFound = () => {
+  const navigate = useNavigate();
+  const goBack = () => {
+    navigate(-1);
+  };
+
   return (
-    <>
-      <h1>404</h1>
-    </>
+    <section className="not-found">
+      <h1 className="not-found__heading">404</h1>
+      <p className="not-found__text">Страница не найдена</p>
+      <Button className="not-found__button" onClick={goBack} type="button">Назад</Button>
+    </section>
   )
 }
 
