928564ca192c95a5f75e6d8037da61b7a9c7527f
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 21:08:39 +0300

    feat: add layout in register component

--- src/components/Register/Register.js
+++ src/components/Register/Register.js
@@ -1,8 +1,32 @@
+import AuthForm from '../AuthForm/AuthForm';
+import Input from '../Input/Input';
+
 const Register = () => {
+  const handleRegister = () => {};
+
   return (
-    <>
-      <h1>Регистрация</h1>
-    </>
+    <AuthForm
+      title="Добро пожаловать!"
+      name="auth-reg"
+      onSubmit={handleRegister}
+      textButton="Зарегистрироваться"
+      textParagraph="Уже зарегистрированы?"
+      textLink="Войти"
+      path="/signin">
+      <Input
+        classNameInput="auth-form__input"
+        classNameLabel="auth-form__label"
+        type="text"
+        name="auth-name"
+        placeholder="Имя"
+        label="Имя"
+        required="required"
+        max="30"
+        min="2"
+        value="Евгения">
+        <span class="auth-form__input-error"></span>
+      </Input>
+    </AuthForm>
   )
 }
 
