c88cb2e5e240951b2ce13dd030a560fb431a8943
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:14:46 +0300

    feat: add moviescard component layout

--- src/components/MoviesCard/MoviesCard.js
+++ src/components/MoviesCard/MoviesCard.js
@@ -1,7 +1,34 @@
-const MoviesCard = () => {
+import { useLocation } from 'react-router-dom';
+import './MoviesCard.css';
+import Button from '../Button/Button';
+
+const MoviesCard = ({ image, title, time, isSave }) => {
+  const location = useLocation();
+
   return (
-    <>
-    </>
+    <li className={
+      location.pathname === "/movies" ?
+      "movies__card movies__card_type_unsaved"
+      :
+      "movies__card movies__card_type_saved"
+      }>
+      <img className="movies__img" src={image} alt={title}></img>
+      <Button
+        className={
+          location.pathname === "/saved-movies" ?
+          "movies__button-save movies__button-save_type_delete"
+          :
+          `movies__button-save ${isSave ? "movies__button-save_type_save" : "movies__button-save_type_choose"}`
+        }
+        type="button"
+        text={!isSave && "Сохранить"} />
+      <div className="movies__container-title">
+        <h2 className="movies__title">{title}</h2>
+        <div className="movies__container-time">
+          <p className="movies__time">{time}</p>
+        </div>
+      </div>
+    </li>
   )
 }
 
