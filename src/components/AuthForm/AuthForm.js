3aed9f472f6847223bea3f46df469acfa8cc60c3
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 20:59:45 +0300

    feat: add layout in authform component

---
+++ src/components/AuthForm/AuthForm.js
@@ -0,0 +1,46 @@
+import { Link } from 'react-router-dom';
+import './AuthForm.css';
+import Form from '../Form/Form';
+import Button from '../Button/Button';
+import Input from '../Input/Input';
+import logo from '../../images/logo.svg';
+
+const AuthForm = ({ title, name, onSubmit, children, textButton, textLink, textParagraph, path, isLoginForm }) => {
+  return (
+    <section className="auth-form">
+      <Link className="auth-form__link auth-form__link_type_logo" to="/"><img className="auth-form__logo" src={logo} alt="logo"></img></Link>
+      <h1 className="auth-form__heading">{title}</h1>
+      <Form className="auth-form__form" onSubmit={onSubmit} name={name}>
+        {children}
+        <Input
+          classNameInput="auth-form__input auth-form__input_type_error"
+          classNameLabel="auth-form__label"
+          type="email"
+          name="auth-email"
+          placeholder="E-mail"
+          required="required"
+          label="E-mail"
+          value="pochta@yandex.ru">
+          <span class="auth-form__input-error">Что-то пошло не так...</span>
+        </Input>
+        <Input
+          classNameInput="auth-form__input auth-form__input_type_error"
+          classNameLabel="auth-form__label"
+          type="password"
+          name="auth-password"
+          placeholder="Пароль"
+          required="required"
+          label="Пароль"
+          value="123456">
+          <span class="auth-form__input-error">Что-то пошло не так...</span>
+        </Input>
+        <div className={`auth-form__container ${isLoginForm && "auth-form__container_type_login"}`}>
+          <Button className="auth-form__button" type="submit" text={textButton} />
+          <p className="auth-form__text">{textParagraph} <Link className="auth-form__link" to={path}>{textLink}</Link></p>
+        </div>
+      </Form>
+    </section>
+  )
+}
+
+export default AuthForm;
