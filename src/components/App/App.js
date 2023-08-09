301f96db4abdf8648875386a4493300fc0bb40fe
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:23:37 +0300

    feat: add protect routes and pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,30 +1,30 @@
+import { useState } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
-import Main from '../Main/Main';
-import Header from '../Header/Header';
-import Register from '../Register/Register';
-import Login from '../Login/Login';
-import NotFound from '../NotFound/NotFound';
-import Footer from '../Footer/Footer';
-import Movies from '../Movies/Movies';
-import SavedMovies from '../SavedMovies/SavedMovies';
-import Profile from '../Profile/Profile';
+import MainPage from '../../pages/MainPage';
+import MoviesPage from '../../pages/MoviesPage';
+import RegisterPage from '../../pages/RegisterPage';
+import LoginPage from '../../pages/LoginPage';
+import NotFoundPage from '../../pages/NotFoundPage';
+import SavedMoviesPage from '../../pages/SavedMoviesPage';
+import ProfilePage from '../../pages/ProfilePage';
+import { ProtectedRouteElementForAuthorizedUser, ProtectedRouteElementForUnauthorizedUser } from '../ProtectedRoutes/ProtectedRoutes';
 
 function App() {
+  const [isLoggedIn, setIsLoggedIn] = useState(true);
+
   return (
     <div className="body">
       <div className="page">
-        <Header />
         <Routes>
-          <Route path="/" element={<Main />} />
-          <Route path="/movies" element={<Movies />} />
-          <Route path="/saved-movies" element={<SavedMovies />} />
-          <Route path="/profile" element={<Profile />} />
-          <Route path="/signup" element={<Register />} />
-          <Route path="/signin" element={<Login />} />
-          <Route path="*" element={<NotFound />} />
+          <Route path="/" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<MainPage />} />}/>
+          <Route path="/movies" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<MoviesPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/saved-movies" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<SavedMoviesPage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/profile" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<ProfilePage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/signup" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<RegisterPage />} />} />
+          <Route path="/signin" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<LoginPage />} />} />
+          <Route path="*" element={<NotFoundPage />} />
         </Routes>
-        <Footer />
       </div>
     </div>
   );
