e4a8ce33faa4ec6bb11e0940169eb7f3bb842a1b
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 21:08:16 +0300

    feat: add layout in login component

--- src/components/Login/Login.js
+++ src/components/Login/Login.js
@@ -1,8 +1,24 @@
-const Login = () => {
+import { useNavigate } from 'react-router-dom';
+import AuthForm from '../AuthForm/AuthForm';
+
+const Login = ({ setIsLoggedIn }) => {
+  const navigate = useNavigate();
+  const handleLogin = () => {
+    setIsLoggedIn(true);
+    navigate('/movies', {replace: true});
+  };
+
   return (
-    <>
-      <h1>Вход</h1>
-    </>
+    <AuthForm
+      title="Рады видеть!"
+      name="auth-log"
+      onSubmit={handleLogin}
+      textButton="Войти"
+      textParagraph="Ещё не зарегистрированы?"
+      textLink="Регистрация"
+      path="/signup"
+      isLoginForm={true}>
+    </AuthForm>
   )
 }
 
