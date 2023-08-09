2bfc21f3da6fbc00bb903833728f3de8802e2b37
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:12:58 +0300

    fix: change header component

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,24 +1,15 @@
-import { useState } from 'react';
-import { Link, Route, Routes, useNavigate } from 'react-router-dom';
+import { Link, Route, Routes, useNavigate, NavLink } from 'react-router-dom';
 import './Header.css';
 import logo from '../../images/logo.svg';
+import Navigation from '../Navigation/Navigation';
 
-const Header = () => {
-  const [isLogin, setLogin] = useState(false);
-  const navigate = useNavigate();
-
+const Header = ({ isLoggedIn }) => {
   return (
-  <header className={`header ${!isLogin && "header_type_main"}`}>
+  <header className={`header ${!isLoggedIn && "header_type_main"}`}>
     <div className="header__container">
       <Link className="header__link header__link_type_logo" to="/"><img className="header__logo" src={logo} alt="logo"></img></Link>
-      {isLogin ?
-        <nav>
-          <ul className="header__list">
-            <li><Link className="header__link" to="/movies">Фильмы</Link></li>
-            <li><Link className="header__link" to="/saved-movies">Сохраненные фильмы</Link></li>
-            <li><Link className="header__link" to="/profile">Аккаунт</Link></li>
-          </ul>
-        </nav>
+      {isLoggedIn ?
+        <Navigation />
         :
         <nav>
           <ul className="header__list">
