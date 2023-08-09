fea275ee11f09b54eea8fa7f14010f7ae04c64f1
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:21:21 +0300

    fix: change movies component

--- src/components/Movies/Movies.js
+++ src/components/Movies/Movies.js
@@ -1,17 +1,13 @@
 import SearchForm from '../SearchForm/SearchForm';
-import FilterCheckbox from '../FilterCheckbox/FilterCheckbox';
 import Preloader from '../Preloader/Preloader';
 import MoviesCardList from '../MoviesCardList/MoviesCardList';
-import MoviesCard from '../MoviesCard/MoviesCard';
 
 const Main = () => {
   return (
     <>
       <SearchForm />
-      <FilterCheckbox />
       <Preloader />
       <MoviesCardList />
-      <MoviesCard />
     </>
   )
 }
