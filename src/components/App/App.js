d7789765736414eca17a062421a5b944fc1552a2
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 21:06:55 +0300

    feat: add props in profile, login pages

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -11,7 +11,7 @@ import ProfilePage from '../../pages/ProfilePage';
 import { ProtectedRouteElementForAuthorizedUser, ProtectedRouteElementForUnauthorizedUser } from '../ProtectedRoutes/ProtectedRoutes';
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(true);
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
 
   return (
     <div className="body">
@@ -20,9 +20,9 @@ function App() {
           <Route path="/" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<MainPage />} />}/>
           <Route path="/movies" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<MoviesPage isLoggedIn={isLoggedIn} />} />} />
           <Route path="/saved-movies" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<SavedMoviesPage isLoggedIn={isLoggedIn} />} />} />
-          <Route path="/profile" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<ProfilePage isLoggedIn={isLoggedIn} />} />} />
+          <Route path="/profile" element={<ProtectedRouteElementForUnauthorizedUser isLoggedIn={isLoggedIn} element={<ProfilePage isLoggedIn={isLoggedIn} setIsLoggedIn={setIsLoggedIn} />} />} />
           <Route path="/signup" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<RegisterPage />} />} />
-          <Route path="/signin" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<LoginPage />} />} />
+          <Route path="/signin" element={<ProtectedRouteElementForAuthorizedUser isLoggedIn={isLoggedIn} element={<LoginPage setIsLoggedIn={setIsLoggedIn} />} />} />
           <Route path="*" element={<NotFoundPage />} />
         </Routes>
       </div>
