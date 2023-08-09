54a7938086a08930a854a92e59b158eaeae44f02
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 03:22:57 +0300

    feat: add draft layout in profile component

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,6 +1,19 @@
+import Form from '../Form/Form';
+import Input from '../Input/Input';
+import Button from '../Button/Button';
+
 const Profile = () => {
+  const handleSubmitEdit = () => {}
+
   return (
     <>
+      <h1>Привет, Евгения</h1>
+      <Form className="profile__form" name="profile" onSubmit={handleSubmitEdit}>
+        <Input classNameInput="profile__input" type="text" />
+        <Input classNameInput="profile__input" type="email" />
+        <Button className="profile__button-edit" type="submit" />
+        <Button className="profile__button-logout" type="button" />
+      </Form>
     </>
   )
 }
