74c24f46c51e48d7f27fd02e3eff20ed310ea24c
EvgeniiyaR <tulpansik@mail.ru>
2023-08-09 21:09:48 +0300

    feat: add parametrs in input component

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,8 @@
-const Input = ({ type, name, placeholder, children, classNameLabel, classNameInput, label }) => {
+const Input = ({ type, name, placeholder, children, classNameLabel, classNameInput, label, value, required, min, max }) => {
   return (
     <label className={classNameLabel}>
       {label}
-      <input className={classNameInput} type={type} name={name} placeholder={placeholder} />
+      <input className={classNameInput} type={type} name={name} placeholder={placeholder} value={value} required={required} min={min} max={max} />
       {children}
     </label>
   )
