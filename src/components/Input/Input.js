3c8a1f76c0b06abfc36fcfef7b9db1ce05fecdd9
EvgeniiyaR <tulpansik@mail.ru>
2023-08-10 15:42:23 +0300

    feat: add parametr disabled

--- src/components/Input/Input.js
+++ src/components/Input/Input.js
@@ -1,8 +1,8 @@
-const Input = ({ type, name, placeholder, children, classNameLabel, classNameInput, label, value, required, min, max }) => {
+const Input = ({ type, name, placeholder, children, classNameLabel, classNameInput, label, value, required, min, max, disabled }) => {
   return (
     <label className={classNameLabel}>
       {label}
-      <input className={classNameInput} type={type} name={name} placeholder={placeholder} value={value} required={required} min={min} max={max} />
+      <input className={classNameInput} type={type} name={name} placeholder={placeholder} value={value} required={required} min={min} max={max} disabled={disabled} />
       {children}
     </label>
   )
