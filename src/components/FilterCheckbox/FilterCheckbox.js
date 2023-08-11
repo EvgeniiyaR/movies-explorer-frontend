745c0f062d2e6e340973e735d635f4702944efcf
EvgeniiyaR <tulpansik@mail.ru>
2023-08-11 18:52:39 +0300

    feat: add focus on filtercheckbox

--- src/components/FilterCheckbox/FilterCheckbox.js
+++ src/components/FilterCheckbox/FilterCheckbox.js
@@ -2,12 +2,16 @@ import './FilterCheckbox.css';
 import Input from '../Input/Input';
 
 const FilterCheckbox = () => {
+  const handleChange = () => {};
+
   return (
     <Input
       classNameLabel="search__label-switch"
       classNameInput="search__input-checkbox"
       type="checkbox"
-      name="search-checkbox">
+      name="search-checkbox"
+      tabIndex="0"
+      onChange={handleChange}>
       <span className="search__slider"></span>
     </Input>
   )
