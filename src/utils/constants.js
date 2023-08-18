024cc39a98d9b6a34fb6ccf2db1d943ca5a24ba1
EvgeniiyaR <tulpansik@mail.ru>
2023-08-18 04:41:00 +0300

    feat: add constants url

--- src/utils/constants.js
+++ src/utils/constants.js
@@ -2,3 +2,8 @@ export const HEADERS = {
   'Accept': 'application/json',
   'Content-Type': 'application/json',
 };
+
+export const BASE_URL_MOVIES_API = 'https://api.nomoreparties.co';
+
+export const BASE_URL = 'http://localhost:3001';
+// export const BASE_URL = 'https://api.movies.evgeniiyar.nomoreparties.sbs';
