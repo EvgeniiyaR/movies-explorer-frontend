05938e0edfd54f5e0aa89acebacd6288070a88fa
EvgeniiyaR <tulpansik@mail.ru>
2023-08-22 02:09:32 +0300

    feat: add handleGetSavedMoviesForDelete, handleGetSavedMoviesForCreate, loading states

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -13,13 +13,15 @@ import InfoTooltip from '../InfoTooltip/InfoTooltip';
 import * as mainApi from '../../utils/MainApi';
 import * as moviesApi from '../../utils/MoviesApi';
 import { CONFLICT, SERVER_ERROR, UNAUTHORIZED, SERVER_ERROR_TEXT, CONFLICT_TEXT, UNAUTHORIZED_TEXT } from '../../utils/errors';
-import { ProtectedRouteElementForAuthorizedUser, ProtectedRouteElementForUnauthorizedUser } from '../ProtectedRoutes/ProtectedRoutes';
+import { ProtectedRouteElementForUnauthorizedUser } from '../ProtectedRoutes/ProtectedRoutes';
 
 const App = () => {
   const navigate = useNavigate();
 
   const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [isLoadingMovies, setIsLoadingMovies] = useState(false);
   const [isLoading, setIsLoading] = useState(false);
+  const [isLoadingSavedMovies, setIsLoadingSavedMovies] = useState(false);
   const [isStatusPopupOpen, setIsStatusPopupOpen] = useState(false);
   const [isEdit, setIsEdit] = useState(false);
   const [status, setStatus] = useState('');
@@ -27,14 +29,16 @@ const App = () => {
   const [movies, setMovies] = useState([]);
   const [filteredMovies, setFilteredMovies] = useState(JSON.parse(localStorage.getItem('filteredMovies')) || []);
   const [savedMovies, setSavedMovies] = useState([]);
-  const [filteredSavedMovies, setFilteredSavedMovies] = useState(JSON.parse(localStorage.getItem('savedFilteredMovies')) || savedMovies);
+  const [filteredSavedMovies, setFilteredSavedMovies] = useState([]);
+  const [isSearchMovies, setIsSearchMovies] = useState(false);
+  const [isSearchSavedMovies, setIsSearchSavedMovies] = useState(false);
 
   const checkUser = () => {
     mainApi.getUserInfo()
     .then((res) => {
       if (res) {
         setIsLoggedIn(true);
-        navigate('/');
+        navigate('/movies');
         setCurrentUser(res);
       } else {
         setIsLoggedIn(false);
@@ -44,17 +48,19 @@ const App = () => {
   };
 
   useEffect(() => {
+    setIsLoadingMovies(true);
     if (isLoggedIn) {
       Promise.all([moviesApi.getAllMovies(), mainApi.getMovies()])
       .then(([movies, savedMovies]) => {
         setMovies(movies);
         setSavedMovies(savedMovies);
-        if (!localStorage.getItem('savedFilteredMovies')) {
-          localStorage.setItem('savedFilteredMovies', JSON.stringify(savedMovies));
-          setFilteredSavedMovies(savedMovies);
-        };
+        setFilteredSavedMovies(savedMovies);
+        setIsLoadingMovies(true);
       })
-      .catch((err) => console.log(`Возникла ошибка: ${err}`));
+      .catch((err) => {
+        setIsLoadingMovies(false);
+        console.log(`Возникла ошибка: ${err}`);
+      });
     }
   }, [isLoggedIn]);
 
@@ -104,7 +110,7 @@ const App = () => {
     });
   };
 
-  const handleUpdateUser = (name, email, isEdit) => {
+  const handleUpdateUser = (name, email) => {
     setIsLoading(true);
     mainApi.editUserInfo(name, email)
     .then((userInfo) => {
@@ -129,29 +135,60 @@ const App = () => {
     .then((res) => {
       if (res) {
         setIsLoggedIn(false);
-        navigate('/signin');
+        navigate('/');
         localStorage.clear();
       }
     })
     .catch((err) => console.log(`Возникла ошибка: ${err}`));
   };
 
-  const handleGetSavedMovies = () => {
+  const handleGetSavedMoviesForDelete = () => {
+    setIsLoadingMovies(true);
+    mainApi.getMovies()
+    .then((res) => {
+      setSavedMovies(res);
+      const movieIdsFromMovies = res.map((item) => item.movieId);
+      const filteredData = filteredSavedMovies
+        .filter((item) => movieIdsFromMovies.includes(item.movieId));
+      setFilteredSavedMovies(filteredData);
+      setIsLoadingMovies(true);
+    })
+    .catch((err) => {
+      setIsLoadingMovies(false);
+      console.log(`Возникла ошибка: ${err}`);
+    });
+  };
+
+  const handleGetSavedMoviesForCreate = () => {
+    setIsLoadingMovies(true);
     mainApi.getMovies()
     .then((res) => {
       setSavedMovies(res);
       const movieIdsFromMovies = movies.map((item) => item.id);
-
       const filteredData = res
         .filter((item) => movieIdsFromMovies.includes(item.movieId));
-
       setFilteredSavedMovies(filteredData);
-      localStorage.setItem('savedFilteredMovies', JSON.stringify(filteredData));
+      setIsLoadingMovies(true);
     })
-    .catch((err) => console.log(`Возникла ошибка: ${err}`));
+    .catch((err) => {
+      setIsLoadingMovies(false);
+      console.log(`Возникла ошибка: ${err}`);
+    });
   };
 
-  const handleCreateMovie = (country, director, duration, year, description, image, trailerLink, thumbnail, nameRU, nameEN, movieId) => {
+  const handleCreateMovie = (
+    country,
+    director,
+    duration,
+    year,
+    description,
+    image,
+    trailerLink,
+    thumbnail,
+    nameRU,
+    nameEN,
+    movieId) => {
+    setIsLoadingMovies(true);
     mainApi.createMovie(
       country,
       director,
@@ -165,17 +202,26 @@ const App = () => {
       nameEN,
       movieId)
     .then(() => {
-      handleGetSavedMovies();
+      setIsLoadingMovies(true);
+      handleGetSavedMoviesForCreate();
     })
-    .catch((err) => console.log(`Возникла ошибка: ${err}`));
+    .catch((err) => {
+      setIsLoadingMovies(false);
+      console.log(`Возникла ошибка: ${err}`);
+    });
   };
 
   const handleDeleteMovie = (id) => {
+    setIsLoadingMovies(true);
     mainApi.deleteMovie(id)
     .then(() => {
-      handleGetSavedMovies();
+      setIsLoadingMovies(true);
+      handleGetSavedMoviesForDelete();
     })
-    .catch((err) => console.log(`Возникла ошибка: ${err}`));
+    .catch((err) => {
+      setIsLoadingMovies(false);
+      console.log(`Возникла ошибка: ${err}`);
+    });
   };
 
   const closeStatusPopup = () => {
@@ -195,11 +241,15 @@ const App = () => {
                 element={<MoviesPage
                   isLoggedIn={isLoggedIn}
                   movies={movies}
+                  isSearchMovies={isSearchMovies}
                   filteredMovies={filteredMovies}
                   setFilteredMovies={setFilteredMovies}
                   savedMovies={savedMovies}
+                  setIsSearchMovies={setIsSearchMovies}
                   handleCreateMovie={handleCreateMovie}
-                  handleDeleteMovie={handleDeleteMovie} />} />} />
+                  handleDeleteMovie={handleDeleteMovie}
+                  isLoading={isLoading}
+                  isLoadingMovies={isLoadingMovies} />} />} />
             <Route
               path="/saved-movies"
               element={<ProtectedRouteElementForUnauthorizedUser
@@ -209,7 +259,12 @@ const App = () => {
                   filteredSavedMovies={filteredSavedMovies}
                   savedMovies={savedMovies}
                   setFilteredSavedMovies={setFilteredSavedMovies}
-                  handleDeleteMovie={handleDeleteMovie} />} />} />
+                  handleDeleteMovie={handleDeleteMovie}
+                  isSearchSavedMovies={isSearchSavedMovies}
+                  setIsSearchSavedMovies={setIsSearchSavedMovies}
+                  isLoadingSavedMovies={isLoadingSavedMovies}
+                  setIsLoadingSavedMovies={setIsLoadingSavedMovies}
+                  isLoadingMovies={isLoadingMovies} />} />} />
             <Route
               path="/profile"
               element={<ProtectedRouteElementForUnauthorizedUser
@@ -225,23 +280,18 @@ const App = () => {
                   setIsEdit={setIsEdit} />} />} />
             <Route
               path="/signup"
-              element={<ProtectedRouteElementForAuthorizedUser
-                isLoggedIn={isLoggedIn}
                 element={<RegisterPage
-                  handleRegister={handleRegister}
-                  status={status}
-                  setStatus={setStatus}
-                  isLoading={isLoading} />}
-              />} />
+                handleRegister={handleRegister}
+                status={status}
+                setStatus={setStatus}
+                isLoading={isLoading} />} />
             <Route
               path="/signin"
-              element={<ProtectedRouteElementForAuthorizedUser
-                isLoggedIn={isLoggedIn}
                 element={<LoginPage
-                  handleLogin={handleLogin}
-                  status={status}
-                  setStatus={setStatus}
-                  isLoading={isLoading} />} />} />
+                handleLogin={handleLogin}
+                status={status}
+                setStatus={setStatus}
+                isLoading={isLoading} />} />
             <Route path="*" element={<NotFoundPage />} />
           </Routes>
         </div>
